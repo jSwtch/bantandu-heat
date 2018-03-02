452b99edf9ee8a802ff5b0bc32dcd23196542eb7
jSwtch <sanderson.jma@gmail.com>
2018-03-02 15:24:05 -0700

    2018 March update

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,20 @@
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/js/app.js");
+
+// CONTACT FORM SLIDER UPDATE
+
+var sliderHeat = document.getElementById("myRange--heat");
+var outputHeat = document.getElementById("update--heat");
+outputHeat.innerHTML = sliderHeat.value;
+
+sliderHeat.oninput = function() {
+    outputHeat.innerHTML = this.value;
+}
+var sliderBottles = document.getElementById("myRange--bottles");
+var outputBottles = document.getElementById("update--bottles");
+outputBottles.innerHTML = sliderBottles.value;
+
+sliderBottles.oninput = function() {
+    outputBottles.innerHTML = this.value;
+}
