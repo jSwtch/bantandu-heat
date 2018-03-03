16b947b0e2541515382715e39f57d29ad2e76546
jSwtch <sanderson.jma@gmail.com>
2018-03-02 17:32:58 -0700

    testing recaptcha

--- src/js/app.js
+++ src/js/app.js
@@ -10,12 +10,31 @@ var outputHeat = document.getElementById("update--heat");
 outputHeat.innerHTML = sliderHeat.value;
 
 sliderHeat.oninput = function() {
-    outputHeat.innerHTML = this.value;
+  outputHeat.innerHTML = this.value;
 }
 var sliderBottles = document.getElementById("myRange--bottles");
 var outputBottles = document.getElementById("update--bottles");
 outputBottles.innerHTML = sliderBottles.value;
 
 sliderBottles.oninput = function() {
-    outputBottles.innerHTML = this.value;
+  outputBottles.innerHTML = this.value;
+}
+
+// GOOGLE INVISIBLE RECAPTCHA
+function YourOnSubmitFn(token) {
+  alert('thanks' + getElementById('firstName').value)
+}
+
+function validate(event) {
+  event.preventDefault();
+  if (!document.getElementById('field').value) {
+    alert("You must add text to the required field");
+  } else {
+    grecaptcha.execute();
+  }
+}
+
+function onload() {
+  var element = document.getElementById('submit');
+  element.onclick = validate;
 }
